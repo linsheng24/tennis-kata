d8a8e185fe27569441164c1d163dda641782fc2b
linsheng24 <linsheng24@gmail.com>
2020-03-01 17:41:08 +0800

    Refactor TennisGame

--- app/TennisGame.php
+++ app/TennisGame.php
@@ -10,17 +10,40 @@ class TennisGame
       return 'Love-All';
     }
 
-    if ($p1_score == 1 && $p2_score == 0) {
-      return 'Fifteen-Love';
-    }
+    // if ($p1_score == 1 && $p2_score == 0) {
+    //   return 'Fifteen-Love';
+    // }
 
-    if ($p1_score == 1 && $p2_score == 2) {
-      return 'Fifteen-Thirty';
-    }
+    // if ($p1_score == 1 && $p2_score == 2) {
+    //   return 'Fifteen-Thirty';
+    // }
 
-    if ($p1_score == 3 && $p2_score == 2) {
-      return 'Forty-Thirty';
+    // if ($p1_score == 3 && $p2_score == 2) {
+    //   return 'Forty-Thirty';
+    // }
+
+    return $this->getTextFromScore($p1_score) . '-' . $this->getTextFromScore($p2_score);
+
+
+  }
+
+  private function getTextFromScore($score)
+  {
+    switch ($score) {
+      case 0:
+        $result = 'Love';
+        break;
+      case 1:
+        $result = 'Fifteen';
+        break;
+      case 2:
+        $result = 'Thirty';
+        break;
+      case 3:
+        $result = 'Forty';
+        break;
     }
+    return $result;
   }
 
 }
